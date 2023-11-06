6eafb6f274da8845cc6fdb846f9e99d727b698b2
vicentefiorito <vicentefiorito@gmail.com>
2023-11-06 10:29:34 -0500

    implemented hit method along with test, might change some later

--- ship.js
+++ ship.js
@@ -1,18 +1,26 @@
 // Ship factory
-const ship = () => {
-    const length = (type) => {
-        let length = 0
-        if(type === 'Carrier') length = 5 
-        else if(type === 'Battleship') length = 4
-        else if(type === 'Cruiser' || type == 'Submarine') length = 3
-        else if(type === 'Destroyer') length = 2
+const ship = (type) => {
+    let shipLength = 0
+    const length = () => {
+        if(type === 'Carrier') shipLength = 5 
+        else if(type === 'Battleship') shipLength = 4
+        else if(type === 'Cruiser' || type == 'Submarine') shipLength = 3
+        else if(type === 'Destroyer') shipLength = 2
         else {
             return 'No ship was found with that type'
         }
-        return length
+        return shipLength
     }
 
-    return {length}
+    const hit = (x) => {
+        // keeps track of the current hits
+        let hits = 0
+        for(let i = 0; i < x; i++) {
+            hits++
+        }
+        return hits
+    }
+    return {length,hit}
 }
 
 module.exports = ship
--- ship.test.js
+++ ship.test.js
@@ -1,10 +1,16 @@
 import ship from "./ship";
-let shipType = ship()
 
 test('Cruiser length should be 3',() => {
-    expect(shipType.length('Cruiser')).toBe(3)
+    expect(ship('Cruiser').length()).toBe(3)
 })
 
 test('No ship found',() => {
-    expect(shipType.length('Battle')).toBe('No ship was found with that type')
+    expect(ship('Battle').length()).toBe('No ship was found with that type')
+})
+
+test('1 hit', () => {
+    expect(ship('Cruiser').hit(1)).toBe(1)
+})
+test('2 hit', () => {
+    expect(ship('Cruiser').hit(2)).toBe(2)
 })
